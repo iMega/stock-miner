f1c2be0b57d0bc4c143dce71f8db02b33d836956
Dmitry Stoletov <irvis@imega.ru>
2021-02-27 16:29:23 +0300

    up deps daemon
    Signed-off-by: Dmitry Stoletov <irvis@imega.ru>

--- deploy/redeploy-stock-miner.sh
+++ deploy/redeploy-stock-miner.sh
@@ -11,18 +11,20 @@ md5_url=$(curl -s -H "Authorization: token $GITHUB_TOKEN" \
     https://api.github.com/repos/imega/stock-miner/releases/tags/$@ | \
     jq -r '.assets[] | select(.name|test("linux-amd64.tar.gz.md5$")) | .url')
 
-curl -H "Authorization: token $token" \
+curl -H "Authorization: token $GITHUB_TOKEN" \
      -H "Accept: application/octet-stream" \
      -sL -o stock-miner.tar.gz \
      $app_url && echo done! || (echo failed to download stock-miner && exit 1)
 
-curl -H "Authorization: token $token" \
+curl -H "Authorization: token $GITHUB_TOKEN" \
      -H "Accept: application/octet-stream" \
      -sL -o stock-miner.tar.gz.md5 \
      $md5_url && echo done! || (echo failed to download stock-miner && exit 1)
 
 echo $(cat stock-miner.tar.gz.md5) stock-miner.tar.gz | md5sum --quiet -c || (echo failed to check md5 sum && exit 1)
 
-tar -xvf stock-miner.tar.gz
+tar --overwrite -xvf stock-miner.tar.gz
 
 rm stock-miner.tar.gz*
+
+systemctl restart stock-miner
--- go.mod
+++ go.mod
@@ -2,4 +2,17 @@ module github.com/imega/stock-miner
 
 go 1.15
 
-require github.com/imega/daemon v0.0.0-20201201122413-e5033e2e5011
+require (
+	github.com/go-chi/chi v1.5.3 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/imega/daemon v0.0.0-20210227131447-65a09a656ac8
+	github.com/magefile/mage v1.11.0 // indirect
+	github.com/sirupsen/logrus v1.8.0 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
+	golang.org/x/sys v0.0.0-20210227040730-b0d1d43c014d // indirect
+	golang.org/x/text v0.3.5 // indirect
+	google.golang.org/genproto v0.0.0-20210226172003-ab064af71705 // indirect
+	google.golang.org/grpc v1.36.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+)
