adafa103f14345a19eaf902e5b9d711c15ad42a6
Dmitry Stoletov <irvis@imega.ru>
2021-03-24 13:19:23 +0300

    add user table and user store manager
    Signed-off-by: Dmitry Stoletov <irvis@imega.ru>

---
+++ storage/create_tables.go
@@ -0,0 +1,74 @@
+package storage
+
+import (
+	"context"
+	"database/sql"
+	"os"
+
+	tools "github.com/imega/stock-miner/sql"
+)
+
+func CreateDatabase(name string) error {
+	if _, err := os.Stat(name); !os.IsNotExist(err) {
+		return nil
+	}
+
+	file, err := os.OpenFile(name, os.O_CREATE, 0666)
+	if err != nil {
+		return err
+	}
+
+	if err := file.Close(); err != nil {
+		return err
+	}
+
+	db, err := sql.Open("sqlite3", name)
+	if err != nil {
+		return err
+	}
+
+	ctx := context.Background()
+	wrapper := tools.TxWrapper{db}
+	wrapper.Transaction(ctx, nil, func(ctx context.Context, tx *sql.Tx) error {
+		if err := priceTable(ctx, tx); err != nil {
+			return err
+		}
+
+		if err := userTable(ctx, tx); err != nil {
+			return err
+		}
+
+		return nil
+	})
+
+	return db.Close()
+}
+
+func priceTable(ctx context.Context, tx *sql.Tx) error {
+	q := `CREATE TABLE IF NOT EXISTS price (
+        symbol VARCHAR(64) NOT NULL,
+        create_at DATETIME NOT NULL,
+        price DECIMAL(10,5) NULL,
+        CONSTRAINT price PRIMARY KEY (symbol, create_at)
+    )`
+
+	_, err := tx.ExecContext(ctx, q)
+
+	return err
+}
+
+func userTable(ctx context.Context, tx *sql.Tx) error {
+	q := `CREATE TABLE IF NOT EXISTS user (
+        email VARCHAR(64) PRIMARY KEY,
+        name VARCHAR(64),
+        avatar VARCHAR(200),
+        id VARCHAR(200),
+        deleted INTEGER DEFAULT 0,
+        role CHAR(4),
+        create_at DATETIME NOT NULL
+    )`
+
+	_, err := tx.ExecContext(ctx, q)
+
+	return err
+}
---
+++ storage/user.go
@@ -0,0 +1,73 @@
+package storage
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"time"
+
+	"github.com/imega/stock-miner/broker"
+	"github.com/imega/stock-miner/contexkey"
+	sqlTool "github.com/imega/stock-miner/sql"
+)
+
+func (s *Storage) GetUser(ctx context.Context) (broker.User, error) {
+	email, ok := contexkey.EmailFromContext(ctx)
+	if !ok {
+		return broker.User{}, fmt.Errorf("failed to extract user from context")
+	}
+
+	q := `select name, avatar, role from user`
+	row := s.db.QueryRowContext(ctx, q, email)
+	var name, avatar, role string
+	if err := row.Scan(&name, &avatar, &role); err != nil {
+		return broker.User{}, fmt.Errorf("failed getting user")
+	}
+
+	return broker.User{
+		Email:  email,
+		Name:   name,
+		Avatar: avatar,
+		Role:   role,
+	}, nil
+}
+
+func (s *Storage) CreateUser(ctx context.Context, user broker.User) error {
+	email, ok := contexkey.EmailFromContext(ctx)
+	if !ok {
+		return fmt.Errorf("failed to extract user from context")
+	}
+
+	wrapper := sqlTool.TxWrapper{s.db}
+
+	wrapper.Transaction(ctx, nil, func(ctx context.Context, tx *sql.Tx) error {
+		userQuery := `insert user (email, name, avatar, id, role, create_at) values(?,?,?,?,?,?)`
+		_, err := tx.ExecContext(ctx, userQuery, email, user.Name, user.Avatar, user.ID, user.Role, time.Now())
+		if err != nil {
+			return fmt.Errorf("failed to create user, %s", err)
+		}
+
+		// settingsQuery := ``
+		// if _, err := tx.ExecContext(ctx, settingsQuery, email, "{}"); err != nil {
+		// 	return fmt.Errorf("failed to create user, %s", err)
+		// }
+
+		return nil
+	})
+
+	return nil
+}
+
+func (s *Storage) RemoveUser(ctx context.Context, user broker.User) error {
+	email, ok := contexkey.EmailFromContext(ctx)
+	if !ok {
+		return fmt.Errorf("failed to extract user from context")
+	}
+
+	q := `update user set delete=1 where email = ?`
+	if _, err := s.db.ExecContext(ctx, q, email); err != nil {
+		return fmt.Errorf("failed to remove user, %s", err)
+	}
+
+	return nil
+}
