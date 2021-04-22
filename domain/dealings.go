42cbffba4ecbb2a820a1b5cb65eb14d75b57d360
Dmitry Stoletov <irvis@imega.ru>
2021-04-22 17:05:51 +0300

    add dealings to domain
    Signed-off-by: Dmitry Stoletov <irvis@imega.ru>

--- domain/dealings.go
+++ domain/dealings.go
@@ -8,6 +8,9 @@ type Transaction struct {
 	SalePrice    float64 `json:"sale_price"`
 	AmountIncome float64 `json:"amount_income"`
 
+	BuyOrderID  string `json:"buy_order_id"`
+	SellOrderID string `json:"sell_order_id"`
+
 	BuyAt    time.Time `json:"buy_at"`
 	Duration int       `json:"duration"`
 	SellAt   time.Time `json:"sell_at"`
