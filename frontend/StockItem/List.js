4fcb0bc76f0b97a6a221b9486babf4271a8095ce
Dmitry Stoletov <irvis@imega.ru>
2021-06-26 22:33:45 +0300

    add work hours for approved stock items
    Signed-off-by: Dmitry Stoletov <irvis@imega.ru>

--- frontend/StockItem/List.js
+++ frontend/StockItem/List.js
@@ -1,37 +1,40 @@
 import React from "react";
 import { useQuery, gql } from "@apollo/client";
-import { PageHeader, Row, Col, Table } from "antd";
+import { PageHeader, Row, Col, Table, InputNumber, Space } from "antd";
 
 import Link from "../StockItemLink";
 
 const List = () => {
-    const { loading, data } = useQuery(StockItemApprovedND);
-    const [ds, setDs] = React.useState([]);
-
-    React.useEffect(() => {
-        if (loading === false && data) {
-            setDs(data.stockItemApproved);
-        }
-    }, [loading, data]);
-
-    const columns = [
+    let ds = [
         {
-            title: "Ticker",
-            dataIndex: "ticker",
-            key: "ticker",
-            render: Link,
+            ticker: "AAPL",
+            figi: "BBG000B9XRY4",
+            amountLimit: 0,
+            transactionLimit: 0,
+            startTime: 10,
+            endTime: 22,
         },
         {
-            title: "FIGI",
-            dataIndex: "figi",
-            key: "figi",
-        },
-        {
-            title: "Limit",
-            dataIndex: "limit",
-            key: "limit",
+            ticker: "AMZN",
+            figi: "BBG000BVPV84",
+            amountLimit: 0,
+            transactionLimit: 0,
+            startTime: 12,
+            endTime: 20,
         },
     ];
+
+    const { loading, data } = useQuery(StockItemApprovedND, {
+        fetchPolicy: "network-only",
+    });
+
+    if (loading === false && data) {
+        ds = data.stockItemApproved;
+    }
+
+    const settingsStockItemHandler = (rec) => {
+        console.log(rec);
+    };
 
     return (
         <PageHeader
@@ -41,12 +44,63 @@ const List = () => {
         >
             <Row>
                 <Col xs={24} lg={12}>
-                    <Table columns={columns} dataSource={ds} />
+                    <Table
+                        loading={loading}
+                        columns={columns(settingsStockItemHandler)}
+                        dataSource={ds}
+                        rowKey="figi"
+                    />
                 </Col>
             </Row>
         </PageHeader>
     );
 };
+
+const columns = (settingsStockItemHandler) => [
+    {
+        title: "Ticker",
+        dataIndex: "ticker",
+        key: "ticker",
+        render: Link,
+    },
+    {
+        title: "FIGI",
+        dataIndex: "figi",
+        key: "figi",
+    },
+    {
+        title: "Work hours",
+        key: "work-hours",
+        render: (_, r) => (
+            <Space>
+                <HourInput
+                    val={r.startTime}
+                    onChange={(v) => {
+                        r.startTime = v;
+                        settingsStockItemHandler(r);
+                    }}
+                />
+                <HourInput
+                    val={r.endTime}
+                    onChange={(v) => {
+                        r.endTime = v;
+                        settingsStockItemHandler(r);
+                    }}
+                />
+            </Space>
+        ),
+    },
+];
+
+const HourInput = ({ val, onChange }) => (
+    <InputNumber
+        min={0}
+        max={23}
+        defaultValue={val}
+        style={{ width: "4em" }}
+        onChange={onChange}
+    />
+);
 
 const StockItemApprovedND = gql`
     query StockItemApproved {
