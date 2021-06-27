ceb9379ad1ca002f9331c26859be5f9ec3672e90
Dmitry Stoletov <irvis@imega.ru>
2021-06-27 12:30:05 +0300

    reorganize code
    Signed-off-by: Dmitry Stoletov <irvis@imega.ru>

--- storage/stock_item_approved_test.go
+++ storage/stock_item_approved_test.go
@@ -12,28 +12,7 @@ import (
 )
 
 func Test_stockItemApprovedTableFieldStartTime(t *testing.T) {
-	db, close, err := helpers.CreateDB(
-		func(ctx context.Context, tx *sql.Tx) error {
-			q := `CREATE TABLE IF NOT EXISTS stock_item_approved (
-                email VARCHAR(64) NOT NULL,
-                ticker VARCHAR(64) NOT NULL,
-                figi VARCHAR(200) NOT NULL,
-                amount_limit FLOAT NOT NULL,
-                transaction_limit INTEGER NOT NULL,
-                currency VARCHAR(64) NOT NULL,
-                CONSTRAINT pair PRIMARY KEY (email, ticker)
-            )`
-
-			if _, err := tx.ExecContext(ctx, q); err != nil {
-				return fmt.Errorf(
-					"failed to create table stock_item_approved, %w",
-					err,
-				)
-			}
-
-			return nil
-		},
-	)
+	db, close, err := helpers.CreateDB(stockItemApprovedCreateTableHelper)
 	if err != nil {
 		t.Fatalf("failed to create database, %s", err)
 	}
@@ -67,4 +46,22 @@ func Test_stockItemApprovedTableFieldStartTime(t *testing.T) {
 	}
 
 	assert.Equal(t, expected, actual)
+}
+
+func stockItemApprovedCreateTableHelper(ctx context.Context, tx *sql.Tx) error {
+	q := `CREATE TABLE IF NOT EXISTS stock_item_approved (
+        email VARCHAR(64) NOT NULL,
+        ticker VARCHAR(64) NOT NULL,
+        figi VARCHAR(200) NOT NULL,
+        amount_limit FLOAT NOT NULL,
+        transaction_limit INTEGER NOT NULL,
+        currency VARCHAR(64) NOT NULL,
+        CONSTRAINT pair PRIMARY KEY (email, ticker)
+    )`
+
+	if _, err := tx.ExecContext(ctx, q); err != nil {
+		return fmt.Errorf("failed to create table stock_item_approved, %w", err)
+	}
+
+	return nil
 }
